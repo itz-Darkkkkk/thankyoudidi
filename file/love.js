078188cd9864e764cd4b9f2975252d38a11064a8
Darkkkk <yashrajrane2@gmail.com>
2025-06-14 23:18:40 +0530

    Update love.js

--- file/love.js
+++ file/love.js
@@ -1,4 +1,4 @@
-﻿(function(window){
+(function(window){
 
     function random(min, max) {
         return min + Math.floor(Math.random() * (max - min + 1));
@@ -169,7 +169,7 @@
             ctx.scale(0.75, 0.75);
             ctx.font = "12px,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
             ctx.fillText("Click Me:) ", 30, -5);
-            ctx.fillText("Birthday Queen !", 28, 10);
+            ctx.fillText("Pretty Little DIDI !", 28, 10);
             ctx.restore();
         },
         clear: function() {
